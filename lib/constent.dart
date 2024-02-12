2d170d0b653023b641d00c7d932b2f69e413c4ec
waheed_ashrf <washraf124@gmail.com>
2024-02-12 11:10:45 +0200

    app constents

---
+++ lib/constent.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff9DE7C0);
+const kSecondColor = Color(0xffFFB039);
+const kBackgroundColor = Color(0xffF5F5F5);
+
+const kTransitionDuration = Duration(milliseconds: 500);
